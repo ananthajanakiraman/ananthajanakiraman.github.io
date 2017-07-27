0c0e8681cbb55417a2675529b58106d09c060c0b
ananthajanakiraman <30395005+ananthajanakiraman@users.noreply.github.com>
2017-07-27 00:39:26 -0500

    Update bitcoinviz.js

--- bitcoinviz.js
+++ bitcoinviz.js
@@ -79,14 +79,12 @@
 
 	   var maximum1 = d3.max(exchange, function(c) { return d3.max(c.values, function(v) {return v.price;})});
  	   var maximum = d3.max(data, function(d) { return d[name]; });
-	   var maximumObj = data.filter(function(d) { return d3.max(exchange, function(c) { return d3.max(d.values, 
-					function(v) {return v.price;})})});
-	       
+	   
            x.domain(d3.extent(data, function(d) {
              return d.date;
            }));
 
-	   console.log(maximum1,maximumObj);
+	   console.log(maximum1,exchange);
 	       
            y.domain([
                 0,
