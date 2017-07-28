c5de87480ff7af4c7161cbbc41e04f7ce811208d
ananthajanakiraman <30395005+ananthajanakiraman@users.noreply.github.com>
2017-07-28 15:24:34 -0500

    Update bitcoinviz.js

--- bitcoinviz.js
+++ bitcoinviz.js
@@ -13,7 +13,7 @@
         
        var parseDate1 = d3.timeFormat("%Y-%m-%d").parse;
        var parseDate  = d3.timeParse("%Y-%m-%d"),
-	   bisectDate = d3.bisector(function(d) { return d.date; }).left;
+	   bisectDate = d3.bisector(function(d) { return d.date; }).right;
        var formatTime = d3.timeFormat("%e %B");
 
        var div = d3.select("body").append("div")
@@ -155,7 +155,7 @@
 
 	    function mousemove() {
 		var x0 = x.invert(d3.mouse(this)[0]),
-	            i = bisectDate(data, x0, 1),
+	            i = bisectDate(data, x0),
 		    d0 = data[i - 1],
 		    d1 = data[i],
 		    d = x0 - d0.date > d1.date - x0 ? d1 : d0;
