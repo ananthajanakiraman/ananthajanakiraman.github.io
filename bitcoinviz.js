e655abb7aaf7a4c98584a1110c7ca60e7be3005d
ananthajanakiraman <30395005+ananthajanakiraman@users.noreply.github.com>
2017-07-27 20:31:35 -0500

    Update bitcoinviz.js

--- bitcoinviz.js
+++ bitcoinviz.js
@@ -90,7 +90,6 @@
 
 	  linedraw.append("path")
 	      .datum(data)
-              .attr("class", "line")
 	      .attr("stroke","#777")
 	      .attr("stroke-width","1.5px")
               .attr("d", line);
