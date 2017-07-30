0dc63003728d1e961a626af2e60383d0ece81e68
ananthajanakiraman <30395005+ananthajanakiraman@users.noreply.github.com>
2017-07-29 23:42:50 -0500

    Update bitcoinviz.js

--- bitcoinviz.js
+++ bitcoinviz.js
@@ -103,7 +103,7 @@
 	     .append("text")
 	     .attr("transform", "rotate(0)")
              .attr("y", 6)
-             .attr("dx", "6em")
+             .attr("dx", "8em")
              .style("text-anchor", "end")
              .text("Closing Price ($)");
 
